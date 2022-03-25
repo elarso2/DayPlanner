6117d1be465fc83247f273f59ee6e8a9960c15f1
Elizabeth Larson <elarso2@uw.edu>
2022-03-24 17:30:11 -0700

    pulling current time from row, added save icon, beginning of function to compare time and adjust class

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 The goal of this assignment is to create an interactive day planner, in which the user enters an event into a time slot, and that events color changes depending on whether the event is in the present, future, or past. Additionally, the curent date and time are displayed at the top of the page, and when the page is refreshed, the content remains because it has been saved to the session storage.
 
-An example of the site can be seen below:
+The site example which we were provided with can be seen below:
 
 ![day planner example](./assets/05-third-party-apis-homework-demo.gif)
 
--- assets/script.js
+++ assets/script.js
@@ -5,4 +5,37 @@ $("#currentDay").text(today.format("MMM Do, YYYY"));
 var time = moment();
 $("#currentTime").text(time.format("h:mm a"));
 
-//
+//allowing user to input an event item
+var eventInput = "";
+
+//saving the event item to local storage
+var saveBtnEl = $(".saveBtn");
+saveBtnEl.on("click", function () {
+  localStorage.getItem("eventInput");
+});
+
+//grabbing time from row elements, and splitting string to obtain just the hour portion of the HTML text
+for (a = 0; a < 10; a++) {
+  var timeRowCurrent = $(".hour")[a].innerHTML;
+  console.log(timeRowCurrent);
+  var currentTime = timeRowCurrent.substr(0, 2);
+  console.log(currentTime);
+  //   var currentTime = [];
+  //   currentTime.push(timeRowCurrent);
+  //   console.log(currentTime);
+}
+
+//compaing hour from above, to the current time collected from moment, to assign a class/style
+function timeCompare() {
+  var eventColorEl = document.getElementsByClassName("event");
+  if (currentTime == moment().format("h")) {
+    eventColorEl.className += "present";
+  } else if (currentTime > moment().format("h")) {
+    eventColorEl.className += "future";
+  } else {
+    eventColorEl.className += "past";
+  }
+  console.log(moment().format("h"));
+}
+
+timeCompare();
